0b07a4e98c9d5fb2957e63dd6ccf82b91b64bcf9
Andrey Kalachev <andrey.kalachev341478od@gmail.com>
2019-11-28 12:37:39 +0200

    made code style amendments

--- Core/RelationCategory.cs
+++ Core/RelationCategory.cs
@@ -5,9 +5,11 @@ namespace OnionApp.Domain.Core
     public class RelationCategory
     {
         public Guid RelationId { get; set; }
+
         public Relation Relation { get; set; }
 
         public Guid CategoryId { get; set; }
+
         public Category Category { get; set; }
     }
 }
