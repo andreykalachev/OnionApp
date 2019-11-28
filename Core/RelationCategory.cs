81b090c128b4a4bafdaacd200b344813a84f754c
Andrey Kalachev <andrey.kalachev341478od@gmail.com>
2019-11-28 10:57:29 +0200

    added models in the way they were created by database first approach

---
+++ Core/RelationCategory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace OnionApp.Domain.Core
+{
+    public class RelationCategory
+    {
+        public Guid RelationId { get; set; }
+        public Guid CategoryId { get; set; }
+    }
+}
